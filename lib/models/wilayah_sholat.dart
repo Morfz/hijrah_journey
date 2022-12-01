0049003d2a0abda493fc3c534fb24757b3eb7769
ARIFIN YUNIANTA <arifinklaten26@gmail.com>
2022-12-01 22:15:22 +0700

    add search wilayah, provider, models

---
+++ lib/models/wilayah_sholat.dart
@@ -0,0 +1,50 @@
+// To parse this JSON data, do
+//
+//     final wilayahSholat = wilayahSholatFromJson(jsonString);
+
+
+import 'dart:convert';
+
+WilayahSholat wilayahSholatFromJson(String str) => WilayahSholat.fromJson(json.decode(str));
+
+String wilayahSholatToJson(WilayahSholat data) => json.encode(data.toJson());
+
+class WilayahSholat {
+    WilayahSholat({
+        required this.status,
+        required this.data,
+    });
+
+    final bool status;
+    final List<Datum> data;
+
+    factory WilayahSholat.fromJson(Map<String, dynamic> json) => WilayahSholat(
+        status: json["status"],
+        data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "status": status,
+        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+    };
+}
+
+class Datum {
+    Datum({
+        required this.id,
+        required this.lokasi,
+    });
+
+    final String id;
+    final String lokasi;
+
+    factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+        id: json["id"],
+        lokasi: json["lokasi"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "lokasi": lokasi,
+    };
+}
--- lib/views/wilayah_sholat.dart
+++ lib/views/wilayah_sholat.dart
@@ -1,68 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:hijrah_journey/providers/wilayah_sholat_provider.dart';
+import 'package:provider/provider.dart';
 
-class WilayahSholatView extends StatelessWidget {
+import '../api/api_waktu_sholat.dart';
+import '../utility/result_state.dart';
+
+class WilayahSholatView extends StatefulWidget {
   const WilayahSholatView({super.key});
 
   @override
+  State<WilayahSholatView> createState() => _WilayahSholatViewState();
+}
+
+class _WilayahSholatViewState extends State<WilayahSholatView> {
+  late String query = "a";
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            const Text('Pilih Wilayah'),
-            IconButton(
-              icon: const Icon(
-                Icons.search,
-                size: 20.0,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/search_resto');
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: SizedBox(
-                    width: 200,
-                    child: Center(
-                      child: Text(
-                        'widget.restaurant.name',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 18.0,
+    return ChangeNotifierProvider(
+      create: (context) => WilayahSholatProvider(
+          apiService: WaktuSholatApiService(), query: query),
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            title:
+                Consumer<WilayahSholatProvider>(builder: (context, state, _) {
+              return TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Cari Wilayah',
+                  icon: Icon(
+                    Icons.search,
+                    size: 20.0,
+                    color: Colors.white,
+                  ),
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+                onChanged: (value) {
+                  state.runSearch(value);
+                },
+              );
+            })),
+        body: Consumer<WilayahSholatProvider>(builder: (context, state, _) {
+          if (state.state == ResultState.loading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state.state == ResultState.hasData) {
+            return ListView.builder(
+                shrinkWrap: true,
+                itemCount: state.list.data.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                              child: SizedBox(
+                                width: 200,
+                                child: Text(
+                                  state.list.data[index].lokasi,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Center(
+                              child: ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Theme.of(context).primaryColor)),
+                                onPressed: () {},
+                                child: const Text('Pilih'),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Center(
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(
-                            Theme.of(context).primaryColor)),
-                    onPressed: () {},
-                    child: const Text('Pilih'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+                  );
+                });
+          } else if (state.state == ResultState.noData) {
+            return Center(
+              child: Material(
+                child: Text(state.message),
+              ),
+            );
+          } else if (state.state == ResultState.error) {
+            return Center(
+              child: Material(
+                child: Text(state.message),
+              ),
+            );
+          } else {
+            return const Center(
+              child: Material(
+                child: Text(''),
+              ),
+            );
+          }
+        }),
       ),
     );
   }
