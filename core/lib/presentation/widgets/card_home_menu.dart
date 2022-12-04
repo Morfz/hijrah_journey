9c5b486a7b2f947bca0faf30f6324d66c45a3c85
Zai <2010817310001@mhs.ulm.ac.id>
2022-12-04 21:19:12 +0800

    Memperbaiki code pada home, menambahkan salam pada home

--- core/lib/presentation/widgets/card_home_menu.dart
+++ core/lib/presentation/widgets/card_home_menu.dart
@@ -1,78 +1,78 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CardHomeMenu extends StatelessWidget {
-  const CardHomeMenu({
+  CardHomeMenu({
     Key? key,
     required this.menu,
   }) : super(key: key);
 
   final List<Map<String, String>> menu;
+  User user = FirebaseAuth.instance.currentUser!;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        shrinkWrap: true,
-        itemCount: menu.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: Image.asset(menu[index]["pict"]!)),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: SizedBox(
-                        width: 150,
-                        child: Center(
-                          child: Text(
-                            menu[index]["nama"]!,
-                            overflow: TextOverflow.ellipsis,
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(padding: const EdgeInsets.all(20),
+            child: Text('Assalamualaikum, ${user.displayName}', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          Expanded(
+            child: ListView.builder(
+              itemCount: menu.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                  child: Card(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            menu[index]['pict']!,
+                            width: 100,
+                            height: 100,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            menu[index]['nama']!,
                             style: const TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 18.0,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: SizedBox(
-                        width: 200,
-                        child: Center(
-                          child: Text(
-                            menu[index]["deskripsi"]!,
-                            textAlign: TextAlign.justify,
-                            overflow: TextOverflow.fade,
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            menu[index]['deskripsi']!,
+                            textAlign: TextAlign.center,
                             style: const TextStyle(
-                              fontSize: 12.0,
+                              fontSize: 12,
                             ),
                           ),
-                        ),
-                      ),
-                    ),
-                    Center(
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStatePropertyAll(
-                                Theme.of(context).primaryColor)),
-                        onPressed: () {
-                          Navigator.pushNamed(context, menu[index]["routes"]!);
-                        },
-                        child: const Text('Pilih'),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+                          Center(
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStatePropertyAll(
+                                      Theme.of(context).primaryColor)),
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                    context, menu[index]["routes"]!);
+                              },
+                              child: const Text('Pilih'),
+                            ),
+                          ),
+                        ]),
+                  ),
+                );
+              },
             ),
-          );
-        });
+          ),
+        ],
+      ),
+    );
   }
 }
