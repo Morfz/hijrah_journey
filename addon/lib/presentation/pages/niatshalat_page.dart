d86f845ad1d4a1fecfd96e8e96266ca900964ded
Zai <2010817310001@mhs.ulm.ac.id>
2022-12-11 02:53:02 +0800

    Update tampilan niat shalat

--- addon/lib/presentation/pages/niatshalat_page.dart
+++ addon/lib/presentation/pages/niatshalat_page.dart
@@ -21,7 +21,8 @@ class NiatShalatPage extends StatelessWidget {
         title: const Text('Niat Shalat'),
       ),
       body: FutureBuilder<String>(
-        future: DefaultAssetBundle.of(context).loadString('assets/data/niatshalat.json'),
+        future: DefaultAssetBundle.of(context)
+            .loadString('assets/data/niatshalat.json'),
         builder: (context, snapshot) {
           final List<NiatShalat> niatshalat = parseNiatShalat(snapshot.data);
           return ListView.builder(
@@ -33,15 +34,70 @@ class NiatShalatPage extends StatelessWidget {
       ),
     );
   }
+
   Widget _buildNiatShalatItem(BuildContext context, NiatShalat niatShalat) {
-    return Card(
-      child: Column(
-        children: <Widget>[
-          Text(niatShalat.name),
-          Text(niatShalat.arabic, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          Text(niatShalat.latin, style: const TextStyle(fontStyle: FontStyle.italic)),
-        ],
-      ),
-    );
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          elevation: 5,
+          margin: const EdgeInsets.all(5),
+          child: Theme(
+            data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+            child: ExpansionTile(
+              title: Text(
+                niatShalat.name,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                          child: Container(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: Text(
+                                niatShalat.arabic,
+                                style: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: Text(
+                                niatShalat.latin,
+                                style: const TextStyle(
+                                    fontSize: 16, fontStyle: FontStyle.italic),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 8, right: 8, top: 5),
+                              child: Text(niatShalat.terjemahan,
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                  )),
+                            )
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
